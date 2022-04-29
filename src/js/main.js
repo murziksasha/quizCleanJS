7bcdbf53fb579af9758f0c06fba36f99c2811e0a
Alexander Grygoriev <truedevelop@yahoo.com>
2022-04-29 14:31:21 +0300

    v1.0 with some bugs

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@
 
 document.addEventListener('DOMContentLoaded',()=>{
 
+
+
   /* All answer questions */
   const option1 = document.querySelector('.option1'),
         option2 = document.querySelector('.option2'),
@@ -20,8 +22,8 @@ document.addEventListener('DOMContentLoaded',()=>{
   let indexOfQuestion, //индекс текущего вопроса
       indexOfPage = 0;  //игдекс текущей страницы
 
-  const answerTracker = document.querySelector('#answers-tracker'),
-        btnNext = document.querySelector('#btn-next');
+  const btnNext = document.querySelector('#btn-next');
+        
 
   /** Score of answers  */
   let score = 0; //итоговой результата викторины
@@ -66,60 +68,114 @@ document.addEventListener('DOMContentLoaded',()=>{
 
   numberOfAllQuestion.innerHTML = questions.length; //выводим кол=во всех вопросов
 
-  const load = (i) => { //отображение вопроса из массива вопросов 
+  const load = () => { //отображение вопроса из массива вопросов 
                         //заполнить пустой див
-    question.innerHTML = questions[i=0].question; //сам текущий вопрос на странице
+    question.innerHTML = questions[indexOfQuestion].question; //сам текущий вопрос на странице
     
-    option1.innerHTML = questions[i].options[0]; //вариатны ответа
-    option2.innerHTML = questions[i].options[1];
-    option3.innerHTML = questions[i].options[2];
-    option4.innerHTML = questions[i].options[3];
+    option1.innerHTML = questions[indexOfQuestion].options[0]; //вариатны ответа
+    option2.innerHTML = questions[indexOfQuestion].options[1];
+    option3.innerHTML = questions[indexOfQuestion].options[2];
+    option4.innerHTML = questions[indexOfQuestion].options[3];
 
-    numberOfQuestion.innerHTML = indexOfPage + 1 //переменная текщуего номера страницы
+    numberOfQuestion.innerHTML = indexOfPage + 1; //переменная текщуего номера страницы
     indexOfPage++;  //увеличение индекса страницы
-  }
+  };
 
   let completedAnswers = []; //созадем массив в который будут записываться уже заданные вопросы
 
+
   const quizOver = () => { //функция завершения игры
-    alert("GAME OVER");
+    document.querySelector('#modal').classList += ` acitve`;
+    correctAnswer.innerHTML = score;
+    nubmerOfAllQuestion2.innerHTML = questions.length;
+    
   }
 
   //функция с рандомным числом чтоб появлялся случайный вопрос:
+
   const randomQuestion = () => {
-    let randomNumber = Math.floor(Math.random() * question.length); //random number from 0 to 3
+    let randomNumber = Math.floor(Math.random() * questions.length); //random number from 0 to 3
     //реализация что б вопросы не повторялись
     let hitDuplicate = false;
-
     //делаем проверку не закончилась ли игра индекс страниц и вопросов равны то все финиш
-    if(indexOfPage === question.length) {
+    if(indexOfPage == questions.length) { //когда кончились вопросы то
       quizOver(); //функция завершения игры
     } else {
       if(completedAnswers.length > 0) {
         completedAnswers.forEach(item => {
-          if(item === randomNumber) {
-            hitDuplicate = true;  //это значит правлиьный ответ!
+          if(item == randomNumber) {
+            hitDuplicate = true;  //проверка на повторнение вопроса
           }
         });
-        if(hitDuplicate) {
+        if(hitDuplicate) { //если повторно один и тот же вопрос
           randomQuestion();
         } else {
           indexOfQuestion = randomNumber;
           load();
         }
       };
-      if(completedAnswers === 0) {
+      if(completedAnswers == 0) {
         indexOfQuestion = randomNumber;
         load();
       }
     };
     completedAnswers.push(indexOfQuestion); //возникшие вопросы будем пушить в проверочный массив для последуюей уникальности
+  };
+
+  const disabledOptions = ()=>{
+
+      optionElements.forEach(item => {
+        item.classList += ` disabled`;//после ответа все другие элементы будут не активные
+        if(item.dataset.id == questions[indexOfQuestion].rightAnswer){
+          item.classList += ` correct`;
+        }
+      });
+  }
+
+  const anableOptions = ()=> {  //отмена классов с ответами и блокированными
+    optionElements.forEach(item => {
+      item.classList -= ` disabled correct wrong`;
+    })
   }
 
 
+  const checkAnswer = el => {
+    if(el.target.dataset.id == questions[indexOfQuestion].rightAnswer){
+      el.target.classList += ` correct`;
+      score++;     
+    } else {
+      el.target.classList += ` wrong`;
+    }
+    disabledOptions();  //функция которая сделает неактивными остальные поля
 
+  }
+
+  for(let k of optionElements){
+    k.addEventListener('click', (e)=>{
+      e.preventDefault();
+      checkAnswer(e);
+    })
+  }
+
+  const validate = () => {  //проверка выбран ли элемент на ответ
+    if(!optionElements[0].classList.contains('disabled')){
+      alert('Выберите вариант ответа пожалуйста!')
+    } else {
+      anableOptions();
+      randomQuestion();
+      
+    }
+  }
+
+  btnNext.addEventListener('click', () => {
+    validate();
+  });
+
+  const tryAgain = () => {
+    window.location.reload();
+  }
+
+  btnTryAgain.addEventListener('click', tryAgain);
 
   randomQuestion();
-  load();
-
 });
